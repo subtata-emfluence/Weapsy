ed5cc73bcb895a76c78f003095ff51d556ee467d
Subrata Sarkar <ssarkar@emfluence.com>
2016-10-12 11:24:10 +0530

    Issue #12 completed: 'Hide permissions if inherits from page is selected'

---
+++ src/Weapsy/wwwroot/js/admin/pages/module-settings.js
@@ -0,0 +1,12 @@
+﻿var inheritPermissions = $("#InheritPermissions");
+var formGroupRoleGrid = $("#form-group-role-grid");
+document.getElementById("InheritPermissions").checked = false;
+
+$(inheritPermissions).bind("click", function () {
+    if ($(inheritPermissions).is(":checked")) {
+        $(formGroupRoleGrid).slideUp(500);
+    }
+    else {
+        $(formGroupRoleGrid).slideDown(500);
+    }
+});
